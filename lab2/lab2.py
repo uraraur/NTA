11c42aefd537cc7866f551b8e08d8e00c4064305
Maria <mmariia.koval@gmail.com>
2023-06-01 22:06:04 +0300

    brbr

---
+++ lab2/lab2.py
@@ -0,0 +1,50 @@
+import time
+import signal
+import sys
+sys.path.insert(0, 'H:/ААА/NTA/lab1/Dockerfilee')
+from lab import *
+
+#Допоміжні функції
+
+
+print(miller_rabin_primality(13546, 10))
+
+def gcd(a, b):
+    r_0, r_1 = a, b 
+    u_0, u_1 = 1, 0
+    v_0, v_1 = 0, 1
+
+    while r_1 != 0:
+        q = r_0 // r_1
+        r_0, r_1 = r_1, r_0 - q*r_1
+        u_0, u_1 = u_1, u_0 - q*u_1
+        v_0, v_1 = v_1, v_0 - q*v_1
+
+    return r_0, u_0, v_0
+
+def inv(a, b):
+    d, u, v = gcd(a, b)
+    if d != 1:
+        return "No inverse exists"
+    return u   
+
+#-------------------------------------
+#Повний перебір:
+
+def brute_force_search(a, b, n):
+    m = 0
+    start_time = time.time()
+    for i in range(n):
+        m = pow(a, i) % n
+        if m == b:
+            return i
+        end_time = time.time()
+        if end_time - start_time > 295:
+            return "Time out!!"
+    return 0 
+
+#-------------------------------------       
+#Алгоритм Сільвера-Поліга-Гелльгама:
+
+def pohlig_hellman_alg(a, b, n):
+    return 1
