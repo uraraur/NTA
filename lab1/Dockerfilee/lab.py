738ada9702d8ef56cf2da12acafc8cfc4e74dcf0
Maria <mmariia.koval@gmail.com>
2023-06-01 22:07:07 +0300

    fg

--- lab1/Dockerfilee/lab.py
+++ lab1/Dockerfilee/lab.py
@@ -285,5 +285,5 @@ def canonical_search(n):
         
     return factors
 
-a = int(input("Input the number you want to factore:"))
+#a = int(input("Input the number you want to factore:"))
 print("\n", canonical_search(a))
