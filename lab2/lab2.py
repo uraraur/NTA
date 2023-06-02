6f5ec1640d642894bf7f1f99dcc6258824935329
Maria <mmariia.koval@gmail.com>
2023-06-02 03:06:05 +0300

    timeout

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,4 +1,5 @@
 import time
+import func_timeout
 import signal
 import sys
 sys.path.insert(0, 'H:/ААА/NTA/lab2')
@@ -72,7 +73,6 @@ def brute_force_search(a, b, n):
 #Алгоритм Сільвера-Поліга-Гелльгама:
 
 def pohlig_hellman_alg(a, b, n):
-    start_time = time.time()
     ord = n - 1
     canon = canonical_search(ord)
     factors = []
@@ -96,10 +96,6 @@ def pohlig_hellman_alg(a, b, n):
             r_p.append(pow(a, (ord * j) // p, n ))
         r.append(r_p)
 
-    end_time = time.time()
-    if end_time - start_time > 295:
-        return "Time out!!"
-
     X = []
     for i in range(len(factors)):
         num = pow(b, ord // factors[i], n)
@@ -114,7 +110,40 @@ def pohlig_hellman_alg(a, b, n):
     return chinese(X, canon)
 
 
-print(pohlig_hellman_alg(483303352902419, 511939775232752, 771816501687809))
+#print(pohlig_hellman_alg(483303352902419, 511939775232752, 771816501687809))
 
+def my_function(n):
+    """Sleep for n seconds and return n squared."""
+    print(f'Processing {n}')
+    time.sleep(n)
+    return n**2
+
+
+def main_controller(max_wait_time, all_data):
+    """
+    Feed my_function with a list of itens to process (all_data).
+
+    However, if max_wait_time is exceeded, return the item and a fail info.
+    """
+    res = []
+    for data in all_data:
+        try:
+            my_square = func_timeout.func_timeout(
+                max_wait_time, my_function, args=[data]
+                )
+            res.append((my_square, 'processed'))
+        except func_timeout.FunctionTimedOut:
+            print('error')
+            res.append((data, 'fail'))
+            continue
+
+    return res
+
+
+timeout_time = 300
+all_data = range(1, 10)  # the data to be processed
+
+res = main_controller(timeout_time, all_data)
+print(res)
 
 
